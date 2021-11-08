7359eb87a76e91c0a2dc1b45e9afbe12a6121cc1
paigeman <53284808+paigeman@users.noreply.github.com>
2021-11-08 14:27:11 +0800

    #commit move spring state machine module to state-module and add cola state machine

---
+++ state-machine/cola-state-machine/src/main/java/org/fade/demo/integrationdemo/colastatemachine/ColaStateMachineApplication.java
@@ -0,0 +1,17 @@
+package org.fade.demo.integrationdemo.colastatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fade
+ * @date 2021/11/08
+ */
+@SpringBootApplication
+public class ColaStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColaStateMachineApplication.class, args);
+    }
+
+}
