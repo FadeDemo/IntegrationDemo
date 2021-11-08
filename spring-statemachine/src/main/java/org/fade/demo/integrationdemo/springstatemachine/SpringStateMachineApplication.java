4405ad97e00d3d86e4a1e9a221ed9e34fb8dc94d
paigeman <53284808+paigeman@users.noreply.github.com>
2021-11-08 10:43:20 +0800

    #commit integrate spring state machine initially

---
+++ spring-statemachine/src/main/java/org/fade/demo/integrationdemo/springstatemachine/SpringStateMachineApplication.java
@@ -0,0 +1,17 @@
+package org.fade.demo.integrationdemo.springstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fade
+ * @date 2021/11/08
+ */
+@SpringBootApplication
+public class SpringStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStateMachineApplication.class, args);
+    }
+
+}
